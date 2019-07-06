51d7105360df01506473ada94c698c72ca7e985c
marcin <uhgugcv@op.pl>
2019-07-06 16:04:59 +0200

    Add change

--- src/main/java/pl/mazur/omernik/biuropodrozy/MainController.java
+++ src/main/java/pl/mazur/omernik/biuropodrozy/MainController.java
@@ -2,15 +2,33 @@ package pl.mazur.omernik.biuropodrozy;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import pl.mazur.omernik.biuropodrozy.Entity.Trip;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello(){
-        return "Hello user!";
+    Set<Trip> trip = new HashSet<>();
+    String someText = "Nowa Wycieczka!";
+
+    @GetMapping("/trip")
+    public ModelAndView getMain() {
+        ModelAndView m = new ModelAndView("index");
+        m.setViewName("index");
+        m.addObject("someText", someText);
+        initTrip();
+        m.addObject("trip", trip);
+        return m;
     }
-    
+
+    public void initTrip() {
+        for (int i = 0; i <= 10; i++) {
+            trip.add(new Trip("Londyn", "London Airport", "continental", "14.08.2019",
+                    "29.08.2019", 1));
+
+        }
+    }
 }
