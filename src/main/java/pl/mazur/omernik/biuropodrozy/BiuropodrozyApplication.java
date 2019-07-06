5742ad312bc96abab42ecdda79e4024560a965b4
marcin <uhgugcv@op.pl>
2019-07-06 11:41:28 +0200

    initial commit

---
+++ src/main/java/pl/mazur/omernik/biuropodrozy/BiuropodrozyApplication.java
@@ -0,0 +1,13 @@
+package pl.mazur.omernik.biuropodrozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiuropodrozyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiuropodrozyApplication.class, args);
+    }
+
+}
