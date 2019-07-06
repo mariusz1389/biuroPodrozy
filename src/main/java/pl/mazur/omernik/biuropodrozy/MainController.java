6e59700df119c936a95ee0bb79025a93393c241a
marcin <uhgugcv@op.pl>
2019-07-06 12:34:23 +0200

    spring security basic conf

---
+++ src/main/java/pl/mazur/omernik/biuropodrozy/MainController.java
@@ -0,0 +1,16 @@
+package pl.mazur.omernik.biuropodrozy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello user!";
+    }
+    
+}
